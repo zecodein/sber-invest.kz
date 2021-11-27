f7b526edc943802f8ae4288a44419889fc57e59e
Yerdaulet <erdauletbatalov@gmail.com>
2021-11-27 20:36:28 +0600

    another one

--- index.js
+++ index.js
@@ -2,10 +2,6 @@
 function handleSubmit () {
     const tarif = document.getElementById('tarif').value;
     const price = document.getElementById('price').value;
-
-    // to set into local storage
-    /* localStorage.setItem("NAME", name);
-    localStorage.setItem("SURNAME", surname); */
     
     sessionStorage.setItem("TARIF", tarif);
     sessionStorage.setItem("PRICE", price);
