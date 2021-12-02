30b1fcd7017ecb897d9d7a078c088891555fa743
Shalqarov <student@ALEM-a1dbe5.alem.school>
2021-12-02 20:39:31 +0600

    branch testing

--- result.js
+++ result.js
@@ -5,13 +5,6 @@ window.addEventListener('load', () => {
     const tarif = params.get('tarif');
     const price = params.get('price'); 
 
-    // Via local Storage
-    /* const name = localStorage.getItem('NAME');
-    const surname = localStorage.getItem('SURNAME'); */
-    
-    // const name = sessionStorage.getItem('NAME');
-    // const surname = sessionStorage.getItem('SURNAME');
-    
     document.getElementById('result-tarif').innerHTML = tarif;
     document.getElementById('result-price').innerHTML = price;
 
