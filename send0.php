8aac3469b3b3ffcebe86413d935b17b89bf1f356
Yerdaulet <erdauletbatalov@gmail.com>
2021-12-16 11:48:05 +0600

    send0.php

--- send0.php
+++ send0.php
@@ -1,6 +1,6 @@
 <?php
 // The message
-if(isset($_POST['submit'])) {
+
 $message = "Line 1\r\nLine 2\r\nLine 3";
 
 // In case any of our lines are larger than 70 characters, we should use wordwrap()
@@ -8,5 +8,5 @@ $message = wordwrap($message, 70, "\r\n");
 
 // Send
 mail('erdauletbatalov@gmail.com', 'My Subject', $message);
-}
+
 ?>
