647a5c20fe068e5da3dc5af0060a7fdb94fab0b0
Li-Khan <alihanzh40@gmail.com>
2022-02-12 15:19:00 +0600

    added pop up for services.html

--- js/form-240.js
+++ js/form-240.js
@@ -1,16 +1,23 @@
 var queryString = location.search.substring(1);
 var tarif = document.getElementById("tarif");
 var tarifOptions = document.getElementById("tarifOptions");
-if (queryString === "min") {
-  tarifOptions.value = "min";
-  $("#price").html("<label >От 20 тыс. тенге*</label>");
-} else if (queryString === "stand") {
+
+var form240min = document.getElementById("form240min");
+var form240stand = document.getElementById("form240stand");
+var form240prem = document.getElementById("form240prem");
+
+form240min.addEventListener("click", function() {
+    tarifOptions.value = "min";
+    $("#price").html("<label >От 20 тыс. тенге*</label>");
+});
+form240stand.addEventListener("click", function() {
   tarifOptions.value = "stand";
   $("#price").html("<label >От 50 тыс. тенге*</label>");
-} else if (queryString === "prem") {
+});
+form240prem.addEventListener("click", function() {
   tarifOptions.value = "prem";
   $("#price").html("<label >От 100 тыс. тенге*</label>");
-}
+});
 
 tarifOptions.addEventListener("change", function () {
   if (tarifOptions.value === "min") {
