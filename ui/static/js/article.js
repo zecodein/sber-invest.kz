d539a66f6a0707fd30afb57b809e93784a340436
quazar <s-bek-k@mail.ru>
2022-07-14 15:16:06 +0600

    implemented CK editor for rich text

--- ui/static/js/article.js
+++ ui/static/js/article.js
@@ -1,11 +1,15 @@
 function createArticle() {
     let title = document.querySelector("#title");
-    let text = document.querySelector("#text");
+    // let text = document.querySelector("#editor");
     let category = document.querySelector("#categories")
+ 
+    // retreive CK editor data from text area
+    const editorData = editor.getData();
+    // console.log(editorData)
 
     var data = JSON.stringify({
         title: title.value,
-        text: text.value,
+        text: editorData, // replaced text by editorData
         category_name: category.value,
     });
 
@@ -22,11 +26,11 @@ function createArticle() {
         });
         let content = await rawResponse.json();
         if (rawResponse.status == 200) {
-            console.log(content)
-
-            let url = "/article/" + content.id
+            console.log(content)      
+              
+              let url = "/article/" + content.id
             // console.log(content, url)
             window.location.replace(url)
         }
-    })();
+    })();    
 }
