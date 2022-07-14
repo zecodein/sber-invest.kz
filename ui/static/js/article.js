9f9d2e0c6f316265c529b033757bd27c0212cbd0
quazar <s-bek-k@mail.ru>
2022-07-14 12:06:39 +0600

    backup commit with quill version

--- ui/static/js/article.js
+++ ui/static/js/article.js
@@ -2,6 +2,8 @@ function createArticle() {
     let title = document.querySelector("#title");
     let text = document.querySelector("#text");
     let category = document.querySelector("#categories")
+ 
+
 
     var data = JSON.stringify({
         title: title.value,
@@ -22,11 +24,11 @@ function createArticle() {
         });
         let content = await rawResponse.json();
         if (rawResponse.status == 200) {
-            console.log(content)
-
-            let url = "/article/" + content.id
+            console.log(content)      
+              
+              let url = "/article/" + content.id
             // console.log(content, url)
             window.location.replace(url)
         }
-    })();
+    })();    
 }
